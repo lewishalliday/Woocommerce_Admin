dc95bac152ac364dd77fe8211bf9d4b57cbdf793
Lewis Halliday <lewis@it-developers.net>
2019-12-06 15:41:33 +0000

    Move constants values to Keychain

--- Woocommerce_Admin/Constants/Constants.swift
+++ Woocommerce_Admin/Constants/Constants.swift
@@ -7,12 +7,11 @@
 //
 
 import Foundation
-
+import SwiftKeychainWrapper
 
 struct Constants {
-    static let CompanyName = "Halliday"
-	static let APIUrl = "https://api.halliday.xyz/wp-json/wc/v3/"
-	static let Auth = ""
-	static let APIKey = "ck_021d9f5309e1d8ebc952582c38545997a40e1bf8"
-	static let APIPassword = "cs_0105a3abe33f2dfae179908351196db816b7fa08"
+	static let CompanyName = KeychainWrapper.standard.string(forKey: "storeName")!
+	static let APIUrl = KeychainWrapper.standard.string(forKey: "apiUrl")!
+	static let APIKey = KeychainWrapper.standard.string(forKey: "apiKey")!
+	static let APIPassword = KeychainWrapper.standard.string(forKey: "apiPassword")!
 }
